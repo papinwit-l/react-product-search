9b62fd91976b386c9f6d402589166589f81f6308
papinwit-l <pae.papinwit@gmail.com>
2024-09-03 09:51:52 +0700

    fix useeffect cleartimeout and add page

--- src/DataList.jsx
+++ src/DataList.jsx
@@ -6,20 +6,28 @@ function DataList(props) {
   const [error, setError] = useState(null);
   const [allProduct, setAllProduct] = useState([]);
   const [skip, setSkip] = useState(0);
-  const [limit, setLimit] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const _LIMIT = 10;
 
   const fetchData = async (item = "") => {
     try {
+      // const resp = await fetch(
+      //   `https://dummyjson.com/products?limit=0&skip=0&select=title,category,price`
+      // );
       const resp = await fetch(
-        `https://dummyjson.com/products?limit=0&skip=0&select=title,category,price`
+        `https://dummyjson.com/products/search?q=${search}&limit=${_LIMIT}&skip=${skip}`
       );
       const data = await resp.json();
-      const result = data.products.filter(
-        (el) =>
-          String(el.title).toLowerCase().includes(search.toLowerCase()) ||
-          String(el.category).toLowerCase().includes(search.toLowerCase()) ||
-          String(el.price).toLowerCase().includes(search.toLowerCase())
-      );
+      console.log(data);
+      setTotal(data.total);
+      const result = data.products;
+      // const result = data.products.filter(
+      //   (el) =>
+      //     String(el.title).toLowerCase().includes(search.toLowerCase()) ||
+      //     String(el.category).toLowerCase().includes(search.toLowerCase()) ||
+      //     String(el.price).toLowerCase().includes(search.toLowerCase())
+      // );
       console.log(result);
       setAllProduct(result);
     } catch (err) {
@@ -28,14 +36,23 @@ function DataList(props) {
       setLoading(false);
     }
   };
+
   useEffect(() => {
-    setTimeout(() => {
+    const timeout = setTimeout(() => {
       fetchData();
     }, 2000);
     return () => {
-      clearTimeout();
+      clearTimeout(timeout);
     };
   }, [search]);
+
+  useEffect(() => {
+    fetchData();
+  }, [skip]);
+
+  const hdlClick = (num) => {
+    setSkip((prev) => prev + num);
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -45,6 +62,11 @@ function DataList(props) {
   }
   return (
     <div>
+      {1 + skip > 1 && <button onClick={() => hdlClick(-10)}>prev</button>}
+      <p>
+        {1 + skip}-{skip + 10 < total ? skip + 10 : total} of {total}
+      </p>
+      {skip + 10 < total && <button onClick={() => hdlClick(10)}>next</button>}
       <ul>
         {allProduct.map((el) => (
           <li>
